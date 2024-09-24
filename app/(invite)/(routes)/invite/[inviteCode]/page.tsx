e6b5ff46bae4e69dd8ef92bdc9c2f1f46ff85ca9
Kamalesh <shanmugakamalesh.mhash8.io>
2024-09-24 19:39:01 +0530

    remove redirect

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,6 +1,5 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { redirectToSignIn } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 interface InviteCodePageProps {
